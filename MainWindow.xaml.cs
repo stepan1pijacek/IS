aa6f3a1e93676305e7c3a1d0a8f4ab6b36a2269b
Štěpán Pijáček <stepan.pijacek@gmail.com>
2021-04-24 16:16:59 +0200

    Design of main window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,12 +26,54 @@ namespace IS
         {
             this.dbContext = dbContext;
             InitializeComponent();
-            GetStudent();
+            //GetStudent();
         }
 
-        private void GetStudent()
+        //private void GetStudent()
+        //{
+        //    StudentGD.ItemsSource = dbContext.Students.ToList();
+        //}
+
+        private void PowerOff_Click(object sender, RoutedEventArgs e)
         {
-            StudentGD.ItemsSource = dbContext.Students.ToList();
+            Application.Current.Shutdown();
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Max_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState is WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void Min_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Table_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
